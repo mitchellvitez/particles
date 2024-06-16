6b035291fb4ea076ee66671ef9d71c627bd7bd40
Mitchell Vitez <mitchell@vitez.me>
2024-06-15 21:15:14 -0700

    more parameter tweaking

--- app/Main.hs
+++ app/Main.hs
@@ -15,6 +15,8 @@ import GHC.Float (int2Float)
 import Data.Time.Clock
 import Data.IORef
 import Control.Concurrent (threadDelay)
+import Debug.Trace
+import System.Random
 
 keyPressed :: GLFW.KeyCallback
 keyPressed window GLFW.Key'Escape _ GLFW.KeyState'Pressed _ = shutdown window
@@ -46,25 +48,40 @@ main = do
   GLFW.setWindowCloseCallback window (Just shutdown)
   glClearColor 0 0 0.1 1 -- dark blue
 
-  oldTime <- newIORef 0
-  particles <- newIORef initialParticles
-
-  let timeScale = 0.001
+  time <- utctDayTime <$> getCurrentTime
+  -- time <- newIORef =<< utctDayTime <$> getCurrentTime
+  -- particles <- newIORef initialParticles
+  ps <- randomize initialParticles
+  loop ps time window
 
   -- main loop
-  forever $ do
-    GLFW.pollEvents
-    time <- utctDayTime <$> getCurrentTime
-    oldTimeValue <- readIORef oldTime
-    particlesValue <- readIORef particles
-    newParticles <- draw window particlesValue (timeScale * (time - oldTimeValue))
-    atomicWriteIORef oldTime time
-    atomicWriteIORef particles newParticles
+  -- TODO: convert this to a loop function without the IORefs
+  -- forever $ do
+  --   GLFW.pollEvents
+  --   time <- utctDayTime <$> getCurrentTime
+  --   oldTimeValue <- readIORef oldTime
+  --   particlesValue <- readIORef particles
+  --   newParticles <- draw window particlesValue (time - oldTimeValue)
+  --   atomicWriteIORef oldTime time
+  --   atomicWriteIORef particles newParticles
 
   GLFW.destroyWindow window
   GLFW.terminate
 
-data Particle = Particle { pos :: Vector2, vel :: Vector2 }
+loop :: [Particle] -> DiffTime -> Window -> IO ()
+loop particles oldTime window = do
+  GLFW.pollEvents
+  time <- utctDayTime <$> getCurrentTime
+  newParticles <- draw window particles (time - oldTime)
+  loop newParticles time window
+
+randomize :: [Particle] -> IO [Particle]
+randomize particles = do
+  r1 <- randomRIO (-1, 1)
+  r2 <- randomRIO (-1, 1)
+  pure $ map (\p -> p { pos = p.pos { x = p.pos.x + r1, y = p.pos.y + r2 } }) particles
+
+data Particle = Particle { pos :: Vector2, vel :: Vector2, density :: Float}
   deriving (Eq, Show)
 
 data Vector2 = Vector2 { x :: Float, y :: Float }
@@ -73,7 +90,7 @@ data Vector2 = Vector2 { x :: Float, y :: Float }
 zeroes :: Vector2
 zeroes = Vector2 0 0
 
-radius = 0.1
+radius = 0.01
 
 drawParticle :: Particle -> IO ()
 drawParticle p = do
@@ -83,6 +100,7 @@ drawParticle p = do
     triangleAmount = 36
     pi = 3.14159365358979
 
+  -- pure $ traceId "drawParticle"
   glBegin GL_TRIANGLE_FAN
   glColor3f 0.25 0.5 1
   glVertex2f x y
@@ -94,43 +112,117 @@ drawParticle p = do
 times :: Vector2 -> Float -> Vector2
 times (Vector2 x y) f = Vector2 (f * x) (f * y)
 
-instance Num Vector2 where
-  a + b = Vector2 (a.x + b.x) (a.y + b.y)
+plus :: Vector2 -> Vector2 -> Vector2
+plus a b = Vector2 (a.x + b.x) (a.y + b.y)
+
+minus :: Vector2 -> Vector2 -> Vector2
+minus a b = Vector2 (a.x - b.x) (a.y - b.y)
 
 negateY :: Vector2 -> Vector2
 negateY (Vector2 x y) = Vector2 x (-y)
 
 down :: Vector2
-down = Vector2 0 1
+down = Vector2 0 (-0.1)
 
 -- returns a point clamped within collision bounds, and a bool for if a hit was detected
-checkCollision :: Float -> Float -> Vector2 -> (Vector2, Bool)
-checkCollision low high (Vector2 x y) = (Vector2 (clampSingle x) (clampSingle y), anyCol)
+resolveCollisions :: Particle -> Particle
+resolveCollisions p = Particle
+  { pos = p.pos
+    { x = clamp p.pos.x
+    , y = clamp p.pos.y
+    }
+  , vel = p.vel
+    { x = if xCollision then -p.vel.x * damping else p.vel.x
+    , y = if yCollision then -p.vel.y * damping else p.vel.y
+    }
+  , density = p.density
+  }
   where
-    clampSingle x = if x < low + radius then low + radius else if x > high - radius then high - radius else x
-    anyCol = x < low + radius || x > high - radius || y < low + radius || y > high - radius
+    low = -1 :: Float
+    high = 1 :: Float
+    damping = 0.7
+    clamp a = if a < low + radius then low + radius else if a > high - radius then high - radius else a
+    xCollision = p.pos.x < low + radius || p.pos.x > high - radius
+    yCollision = p.pos.y < low + radius || p.pos.y > high - radius
 
-updateParticle :: Particle -> DiffTime -> Particle
-updateParticle p deltaTime =
-  let
-    newPos = p.pos + (p.vel `times` realToFrac deltaTime)
-    collision = checkCollision (-1) 1 $ newPos
-    newVel = p.vel + (down `times` gravity `times` realToFrac deltaTime)
-  in
-    Particle
-      { pos = fst collision
-      , vel = if snd collision then negateY newVel else newVel
-      }
+updateParticle :: [Particle] -> Particle -> DiffTime -> Particle
+updateParticle allParticles p deltaTime =
+  {-trace "updateParticle"-} resolveCollisions $ Particle
+    { pos = p.pos `plus` (newVel `times` realToFrac deltaTime)
+    , vel = newVel `plus` (pressureAcceleration `times` realToFrac deltaTime)
+    , density = calculateDensity allParticles p.pos
+    }
+  where
+    newVel = p.vel `plus` (down `times` (gravity * realToFrac deltaTime))
+    pressureAcceleration = pressureForce allParticles p.pos `times` (1 / p.density)
 
-gravity = -9.81
+initialParticles :: [Particle]
+initialParticles = [Particle (Vector2 x y) zeroes 1 | x <- range, y <- range]
+  where
+    range = [-0.9, -0.8 .. 0.9]
 
-initialParticles = [Particle zeroes zeroes]
+smooth :: Float -> Float -> Float
+smooth radius dst =
+  let volume = pi * radius^8 / 4
+  in if dst > radius then 0 else (radius - dst) * (radius - dst)/ volume
+
+smoothDerivative :: Float -> Float -> Float
+smoothDerivative dst radius =
+  if dst >= radius then 0 else scale * (dst - radius)
+  where
+    scale = 12 / (radius^4 * pi)
+
+magnitude :: Vector2 -> Float
+magnitude (Vector2 x y) = sqrt (x*x + y*y)
+
+sumVec :: [Vector2] -> Vector2
+sumVec = foldl plus zeroes
+
+right :: Vector2
+right = Vector2 1 0
+
+up :: Vector2
+up = Vector2 0 1
+
+mass = 1
+smoothingRadius = 0.5
+
+targetDensity = 100000
+pressureMultiplier = 10
+gravity = 1
+
+pressureForce :: [Particle] -> Vector2 -> Vector2
+pressureForce allParticles samplePoint =
+  sumVec $ (flip map) (filter notThisPos allParticles) $ \p ->
+    let
+      dst = magnitude (p.pos `minus` samplePoint)
+      dir = (p.pos `minus` samplePoint) `times` (1 / dst)
+      slope = smoothDerivative smoothingRadius dst
+      -- dens = calculateDensity allParticles p.pos
+      dens = p.density
+    in
+      dir `times` (-1 * densityToPressure dens * slope * mass / dens)
+  where
+    notThisPos :: Particle -> Bool
+    notThisPos p = not $ p.pos == samplePoint
+
+densityToPressure :: Float -> Float
+densityToPressure dens = densityError * (-pressureMultiplier)
+  where
+    densityError = dens - targetDensity
+
+calculateDensity :: [Particle] -> Vector2 -> Float
+calculateDensity allParticles samplePoint =
+  sum $ (flip map) allParticles $ \p ->
+    let dst = magnitude (p.pos `minus` samplePoint)
+        influence = smooth smoothingRadius dst
+    in mass * influence
 
 draw :: Window -> [Particle] -> DiffTime -> IO [Particle]
 draw window oldParticles deltaTime = do
-  let particles = map (\particle -> updateParticle particle deltaTime) oldParticles
+  let particles = map (\particle -> updateParticle oldParticles particle deltaTime) oldParticles
 
-  -- putStrLn $ "deltaTime = " <> show deltaTime
+  putStrLn $ "deltaTime = " <> show deltaTime
 
   glClear GL_COLOR_BUFFER_BIT
 
--- dist-newstyle/build/x86_64-osx/ghc-9.8.2/fluidsim-0.1.0.0/x/fluidsim/build/fluidsim/autogen/cabal_macros.h
+++ dist-newstyle/build/x86_64-osx/ghc-9.8.2/fluidsim-0.1.0.0/x/fluidsim/build/fluidsim/autogen/cabal_macros.h
@@ -50,6 +50,16 @@
   (major1) == 4 && (major2) <  19 || \
   (major1) == 4 && (major2) == 19 && (minor) <= 1)
 #endif /* MIN_VERSION_base */
+/* package random-1.2.1.2 */
+#ifndef VERSION_random
+#define VERSION_random "1.2.1.2"
+#endif /* VERSION_random */
+#ifndef MIN_VERSION_random
+#define MIN_VERSION_random(major1,major2,minor) (\
+  (major1) <  1 || \
+  (major1) == 1 && (major2) <  2 || \
+  (major1) == 1 && (major2) == 2 && (minor) <= 1)
+#endif /* MIN_VERSION_random */
 /* package time-1.12.2 */
 #ifndef VERSION_time
 #define VERSION_time "1.12.2"
